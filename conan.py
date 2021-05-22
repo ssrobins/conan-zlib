f81300de83e6d7f54fd9d3921e932a663fce6559
Steve Robinson <ssrobins@gmail.com>
2021-05-22 15:31:09 -0700

    Simplify conan command

--- conan.py
+++ conan.py
@@ -6,10 +6,10 @@ import subprocess
 
 def main():
     platform = {
-        "androidarm": "-s os=Android -s os.api_level=16 -s arch=armv7 -s compiler=clang -s compiler.version=11 -s compiler.libcxx=libc++",
-        "androidarm64": "-s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=11 -s compiler.libcxx=libc++",
+        "androidarm": "-s os=Android -s os.api_level=16 -s arch=armv7 -s compiler=clang -s compiler.version=11",
+        "androidarm64": "-s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=11",
         "ios": "-s os=iOS -s arch=armv7 -s os.version=9.0 -s compiler.version=12.0",
-        "linux": "-s compiler.libcxx=libstdc++11",
+        "linux": "",
         "macos": "-s os.version=10.9 -s compiler.version=12.0",
         "windows": "-s arch=x86 -s compiler.version=16 -s compiler.runtime=MT"
     }
