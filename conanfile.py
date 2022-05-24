f98bf7d9eab219a3173211a14ffd5c0cb3543b2c
Steve Robinson <ssrobins@gmail.com>
2022-05-24 15:42:32 -0700

    Switch to CMakeDeps generator

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
-from conans import ConanFile, CMake, tools
-from conans.util import files
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
 import os
 
 class Conan(ConanFile):
@@ -13,8 +13,7 @@ class Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    generators = "cmake"
-    _cmake = None
+    generators = "CMakeDeps"
     revision_mode = "scm"
     exports_sources = ["CMakeLists.diff", "CMakeLists.txt"]
     zip_folder_name = f"{name}-{version}"
@@ -30,26 +29,22 @@ class Conan(ConanFile):
             sha256="91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9")
         os.rename(self.zip_folder_name, self.source_subfolder)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-        self._cmake = CMake(self)
-        if self.settings.os != "Windows":
-            self._cmake.generator = "Ninja Multi-Config"
-        if self.settings.os == "Android":
-            self._cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = os.getenv("ANDROID_NDK_ROOT") + "/build/cmake/android.toolchain.cmake"
-        elif self.settings.os == "iOS" and self.settings.arch != "x86_64":
-            self._cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = "armv7;arm64"
-        elif self.settings.os == "Windows" and self.settings.arch == "x86":
-            self._cmake.generator_platform = "Win32"
-        self._cmake.configure(build_dir=self.build_subfolder)
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja Multi-Config"
+        tc.variables["CMAKE_VERBOSE_MAKEFILE"] = "TRUE"
+        if self.settings.os == "iOS" and self.settings.arch != "x86_64":
+            tc.blocks["apple_system"].values["cmake_osx_architectures"] = "armv7;arm64"
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build(args=["--verbose"])
-        with tools.chdir(self.build_subfolder):
-            self.run(f"ctest -C {self.settings.build_type} --output-on-failure")
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        #with tools.chdir(self.build_subfolder):
+        #     self.run(f"ctest -C {self.settings.build_type} --output-on-failure")
 
     def package(self):
         self.copy("*.h", dst="include", src=self.source_subfolder)
